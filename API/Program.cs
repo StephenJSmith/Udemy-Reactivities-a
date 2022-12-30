69857c9c96c44b8806480a0ab63efb67f9e89d28
Stephen Smith <stephensmith_aus@yahoo.com.au>
2022-12-31 07:26:31 +1100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,16 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// builder.Services.AddCors(opt => {
+//     opt.AddPolicy("CorsPolicy", policy => {
+//         policy
+//             .AllowAnyMethod()
+//             .AllowAnyHeader()
+//             .WithOrigins("http://localhost:3000");
+//     });
+// });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// app.UseCors("CorsPolicy");
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseAuthorization();
 
 app.MapControllers();
@@ -30,9 +42,9 @@ using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
 {
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    // var context = services.GetRequiredService<DataContext>();
+    // await context.Database.MigrateAsync();
+    // await Seed.SeedData(context);
 }
 catch (System.Exception ex)
 {
